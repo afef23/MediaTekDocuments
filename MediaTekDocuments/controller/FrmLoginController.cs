3c52da589b6c88f22ebc34a618a5f349e5fd9a48
afef23 <135067728+afef23@users.noreply.github.com>
2024-04-15 00:21:14 +0200

    Initial commit

---
+++ MediaTekDocuments/controller/FrmLoginController.cs
@@ -0,0 +1,78 @@
+﻿using MediaTekDocuments.model;
+using MediaTekDocuments.dal;
+using System.Security.Cryptography;
+using System.Text;
+using MediaTekDocuments.view;
+
+namespace MediaTekDocuments.controller
+{
+    class FrmLoginController
+    {
+        /// <summary>
+        /// Objet d'accès aux données
+        /// </summary>
+        private readonly Access access;
+
+        private Utilisateur utilisateur = null;
+
+
+        /// <summary>
+        /// Récupération de l'instance unique d'accès aux données
+        /// </summary>
+        public FrmLoginController()
+        {
+            access = Access.GetInstance();
+        }
+
+        /// <summary>
+        /// Lance la vue principale
+        /// </summary>
+
+
+        /// <summary>
+        /// Retourne vrai si l'utilisateur existe dans la BDD
+        /// </summary>
+        /// <param name="ident"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool GetConnexion(string ident, string password)
+        {
+            password = "salt123" + password;
+            string hash = "";
+
+            hash = ComputeSHA256Hash(password);
+
+            utilisateur = access.GetConnexion(ident, hash);
+            if (utilisateur != null)
+            {
+                FrmMediatek mediatek = new FrmMediatek(utilisateur);
+                mediatek.Show();
+                return true;
+            }
+            else { return false; }
+
+        }
+
+        /// <summary>
+        /// Retourne le hash au format string
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string ComputeSHA256Hash(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    stringBuilder.Append(b.ToString("x2"));
+                }
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
---
+++ MediaTekDocuments/view/FrmLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MediaTekDocuments.controller;
+using System.Windows.Forms;
+using MediaTekDocuments.model;
+namespace MediaTekDocuments.view
+{
+    public partial class FrmLogin : Form
+    {
+        private FrmLoginController controller;
+        public FrmLogin()
+        {
+            InitializeComponent();
+            controller = new FrmLoginController();
+            this.AcceptButton = btnConnec;
+        }
+
+
+
+        /// <summary>
+        /// Demande de connection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnConnec_Click(object sender, EventArgs e)
+        {
+            if (controller.GetConnexion(txbLogin.Text, txbPwd.Text))
+            {
+                this.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Mauvais mdp ou identifiant utilisateur");
+
+            }
+
+        }
+    }
+}
